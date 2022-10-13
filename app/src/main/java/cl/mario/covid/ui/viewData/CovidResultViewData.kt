bbad1c73417b5cc4e5bfb3ee31c0e74f60de1732
mariodelgado <mario.delgado@carsales.com>
2022-10-13 14:11:16 -0300

    split project in modules

--- app/src/main/java/cl/mario/covid/ui/viewData/CovidResultViewData.kt
+++
@@ -1,9 +0,0 @@
-package cl.mario.covid.ui.viewData
-
-import cl.mario.covid.util.getCurrentDateWithSpanishFormat
-
-data class CovidResultViewData(
-    val confirmed: Int = 0,
-    val death: Int = 0,
-    val date: String = getCurrentDateWithSpanishFormat()
-)
